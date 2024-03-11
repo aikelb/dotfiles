4c289acc92f8a8b16c757879cb5deb2e8ae377e3
Aitor Lozano <info@aitorlozano.com>
2024-03-11 14:21:25 +0000

    Initial commit

---
+++ dot_config/nvim/lua/custom/init.lua
@@ -0,0 +1,28 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
+
+-- Lua lsp for script files
+autocmd("BufNewFile", {
+  pattern = "*.script,*.gui_script",
+  command = "setfiletype lua",
+})
+autocmd("BufRead", {
+  pattern = "*.script,*.gui_script",
+  command = "setfiletype lua",
+})
+
+-- hlsl for defold shader files
+autocmd("BufNewFile", {
+  pattern = "*.fp,*.vp",
+  command = "setfiletype hlsl",
+})
+autocmd("BufRead", {
+  pattern = "*.fp,*.vp",
+  command = "setfiletype hlsl",
+})
+-- vim.cmd("autocmd! BufNewFile,BufRead *.script setfiletype lua")
