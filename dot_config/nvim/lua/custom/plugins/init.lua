e54da4c3fd3aff2c46919e36d914b823c30721db
Aitor Lozano <info@aitorlozano.com>
2024-03-16 15:51:19 +0000

    Init file plugins

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,49 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvim-pack/nvim-spectre',
+    event = 'BufRead',
+    config = function()
+      require('spectre').setup()
+    end,
+  },
+
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+    },
+    config = function()
+      require('neo-tree').setup {
+        close_if_last_window = true,
+        window = {
+          width = 30,
+        },
+        buffers = {
+          follow_current_file = { enabled = true },
+        },
+        filesystem = {
+          follow_current_file = { enabled = true },
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_by_name = {
+              'node_modules',
+            },
+            never_show = {
+              '.DS_Store',
+              'thumbs.db',
+            },
+          },
+        },
+      }
+    end,
+  },
+
+  { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons' },
+}
