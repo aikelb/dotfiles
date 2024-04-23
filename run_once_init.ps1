ce7be811c3a81f26268833d1808d6b63abdfdd98
Aitor Lozano <info@aitorlozano.com>
2024-04-23 17:43:32 +0100

    Remove redundant app

--- run_once_init.ps1
+++ run_once_init.ps1
@@ -11,9 +11,9 @@ scoop uninstall vcredist2022
 
 $delNvChad = Read-Host "Delete previous nvim setup? (yN)"
 if ($delNvChad -eq "y" -or $delNvChad -eq "Y") {
-    Write-Host "Cleaning AppData\Local\nvim..."
-    rm -Force ~\AppData\Local\nvim
-    rm -Force ~\AppData\Local\nvim-data
+		Write-Host "Cleaning AppData\Local\nvim..."
+		rm -Force ~\AppData\Local\nvim
+		rm -Force ~\AppData\Local\nvim-data
 }
 
 Write-Host "Creating links..."
@@ -22,30 +22,30 @@ $symLinkPath = Join-Path $(Resolve-Path ~/AppData/Local).Path "nvim"
 $actualNvimConfigPath = $(Resolve-Path ~/.config/nvim).Path
 if (!$(Test-Path $symLinkPath) -and $(Test-Path $actualNvimConfigPath))
 {
-    gsudo cmd /c mklink /d $symLinkPath $actualNvimConfigPath
+		gsudo cmd /c mklink /d $symLinkPath $actualNvimConfigPath
 }
 
 # To list all appx packages:
 # Get-AppxPackage | Format-Table -Property Name,Version,PackageFullName
 Write-Header "Removing UWP Apps..."
 $uwpApps = @(
-    "Microsoft.OutlookForWindows",
-    "Microsoft.BingSearch",
-    "Microsoft.BingWeather",
-    "Microsoft.WindowsFeedbackHub",
-    "Microsoft.YourPhone",
-    "Microsoft.MicrosoftOfficeHub",
-    "Microsoft.People",
-    "Microsoft.GetHelp",
-    "Microsoft.BingNews",
-    "Microsoft.BingNews_8wekyb3d8bbwe")
+		"Microsoft.OutlookForWindows",
+		"Microsoft.BingSearch",
+		"Microsoft.BingWeather",
+		"Microsoft.WindowsFeedbackHub",
+		"Microsoft.YourPhone",
+		"Microsoft.MicrosoftOfficeHub",
+		"Microsoft.People",
+		"Microsoft.GetHelp",
+		"Microsoft.BingNews",
+)
 
 $delW11Packages = Read-Host "Delete W11 preinstalled pacakges? (yN)"
 if ($delW11Packages -eq "y" -or $delW11Packages -eq "Y") {
-	foreach ($uwp in $uwpApps)
-	{
-    		gsudo Get-AppxPackage -Name $uwp | Remove-AppxPackage
-	}
+		foreach ($uwp in $uwpApps)
+		{
+				gsudo Get-AppxPackage -Name $uwp | Remove-AppxPackage
+		}
 }
 
 Write-Host "Done! Run nvim to finish installing modules and run :MasonInstallAll"
