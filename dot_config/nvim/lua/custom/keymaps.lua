7bf163dc3f8f611054346e42ffdba2fec9d3cde5
Aitor <aike89@gmail.com>
2024-05-03 14:57:04 +0100

    Update keymaps.lua

--- dot_config/nvim/lua/custom/keymaps.lua
+++ dot_config/nvim/lua/custom/keymaps.lua
@@ -17,6 +17,10 @@ map("n", "<leader>wl", ":NeovimProjectLoadRecent<CR>", { desc = "[W]orkspace [L]
 -- Telescope file browser
 map("n", "<leader>fb", ":Telescope file_browser<CR>", { desc = "[F]ile [B]rowser" })
 map("n", "<leader>fp", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { desc = "[File] Browser Buffer [P]ath" })
+
+-- Trailing spaces
+map("n", "<leader>ft", ":%s/\\s\\+$//e<CR>", {desc = "[F]ormat [T]railing spaces"})
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
