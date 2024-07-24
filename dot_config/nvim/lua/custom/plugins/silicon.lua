d7fff2cf0efd20c72199b71459bec60d4c59e55b
Aitor Lozano <info@aitorlozano.com>
2024-07-24 09:21:04 +0100

    kickstart update

--- dot_config/nvim/lua/custom/plugins/silicon.lua
+++ dot_config/nvim/lua/custom/plugins/silicon.lua
@@ -1,20 +1,14 @@
 return {
-	"michaelrommel/nvim-silicon",
-	lazy = true,
-	cmd = "Silicon",
-	init = function()
-		local wk = require("which-key")
-		wk.register({
-			["<leader>sc"] = { ":Silicon<CR>", "Snapshot Code" },
-		}, { mode = "v" })
-	end,
-	config = function()
-		require("silicon").setup({
-			font = "FiraCode Nerd Font=34",
-			theme = "Dracula",
-			background = "#94e2d5",
-			to_clipboard = true,
-			gobble = true,
-		})
-	end,
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    config = function()
+        require("silicon").setup({
+            font = "FiraCode Nerd Font=34",
+            theme = "Dracula",
+            background = "#94e2d5",
+            to_clipboard = true,
+            gobble = true,
+        })
+    end,
 }
