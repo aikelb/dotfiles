6de1f6135948e309cdc63aeb060299195d550127
Aitor Lozano <info@aitorlozano.com>
2024-12-06 08:52:32 +0000

    css lang

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -950,7 +950,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'astro', 'glsl', 'textproto' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'glsl', 'textproto', 'astro', 'css' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
